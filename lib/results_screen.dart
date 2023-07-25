45ecd5646cc564d71ca708ccfb3ea6bdc82b0160
Lucas Nakamura <105315899+lucasitiro@users.noreply.github.com>
2023-07-25 17:10:11 -0300

    Update results_screen.dart

--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -9,7 +9,7 @@ class ResultsScreen extends StatelessWidget {
     for (var i = 0; i<chosenAnswer.length; i++){
       summary.add({
         'question_index: ': i,
-         'question': questions[i],
+         'question': questions[i].text,
          'correct_answer': questions[i].answers[0],
          'user_answer': chosenAnswer[i]
          });
