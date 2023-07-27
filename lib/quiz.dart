2ea4c1fc6fc514173ccf7ea4bf892c244e5cb89c
Lucas Nakamura <105315899+lucasitiro@users.noreply.github.com>
2023-07-27 16:18:12 -0300

    Update quiz.dart

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -4,8 +4,9 @@ import 'package:quiz_app/start_screen.dart';
 import 'package:quiz_app/data/questions.dart';
 import 'package:quiz_app/results_screen.dart';
 
-class Quiz extends StatefulWidget{
+class Quiz extends StatefulWidget {
   const Quiz({super.key});
+
   @override
   State<Quiz> createState() {
     return _QuizState();
@@ -13,52 +14,64 @@ class Quiz extends StatefulWidget{
 }
 
 class _QuizState extends State<Quiz> {
-  List<String> selectedAnswers = [];
-  var activeScreen = 'start-screen'; 
-  
-  void switchScreen(){
-    setState((){
-      activeScreen = 'questions-screen';
-   }) ;
+  final List<String> _selectedAnswers = [];
+  var _activeScreen = 'start-screen';
+
+  void _switchScreen() {
+    setState(() {
+      _activeScreen = 'questions-screen';
+    });
   }
-  void chooseAnswer(String answer){
-    selectedAnswers.add(answer);
-    if (selectedAnswers.length == questions.length){
+
+  void _chooseAnswer(String answer) {
+    _selectedAnswers.add(answer);
+
+    if (_selectedAnswers.length == questions.length) {
       setState(() {
-        
-        activeScreen = 'results-screen';
+        _activeScreen = 'results-screen';
       });
     }
   }
 
-   
-  
+  void restartQuiz() {
+    setState(() {
+      _activeScreen = 'questions-screen';
+    });
+  }
+
   @override
   Widget build(context) {
-    Widget screenWidget = StartScreen(switchScreen);
-    
-    if (activeScreen == 'questions-screen'){
-      screenWidget =  QuestionsScreen(onSelectAnswer: chooseAnswer,);
+    Widget screenWidget = StartScreen(_switchScreen);
+
+    if (_activeScreen == 'questions-screen') {
+      screenWidget = QuestionsScreen(
+        onSelectAnswer: _chooseAnswer,
+      );
     }
-    if (activeScreen == 'results-screen'){
-      screenWidget =  ResultsScreen(chosenAnswer: selectedAnswers,);
-    };
+
+    if (_activeScreen == 'results-screen') {
+      screenWidget = ResultsScreen(
+        chosenAnswers: _selectedAnswers,
+        onRestart: restartQuiz,
+      );
+    }
+
     return MaterialApp(
-    home: Scaffold(
-      body:Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            colors: [
-               Color.fromARGB(255, 78, 14, 134),
-               Color.fromARGB(255, 107, 15, 168),
-            ],
-            begin:Alignment.topLeft,
-            end: Alignment.bottomRight,
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 78, 13, 151),
+                Color.fromARGB(255, 107, 15, 168),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
           ),
-          ) ,
-        child: screenWidget
+          child: screenWidget,
         ),
-    ),
-  );
+      ),
+    );
   }
 }
